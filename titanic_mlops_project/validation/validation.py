b8fc1b3149ff16aa60a741d24a920689992cfd54
benmackenzie <ben.mackenzie@databriks.com>
2023-11-21 09:27:20 -0500

    fix

--- titanic_mlops_project/validation/validation.py
+++ titanic_mlops_project/validation/validation.py
@@ -7,14 +7,7 @@ from mlflow.models import make_metric, MetricThreshold
 def custom_metrics():
 
     # TODO(optional) : define custom metric function to be included in custom_metrics.
-    def squared_diff_plus_one(eval_df, _builtin_metrics):
-        """
-        This example custom metric function creates a metric based on the ``prediction`` and
-        ``target`` columns in ``eval_df`.
-        """
-        return np.sum(np.abs(eval_df["prediction"] - eval_df["target"] + 1) ** 2)
-
-    return [make_metric(eval_fn=squared_diff_plus_one, greater_is_better=False)]
+    return []
 
 
 # Define model validation rules. Return empty dict if validation rules are not needed.
@@ -22,14 +15,9 @@ def custom_metrics():
 # TODO(optional) : validation_thresholds
 def validation_thresholds():
     return {
-        "max_error": MetricThreshold(
-            threshold=500, higher_is_better=False  # max_error should be <= 500
-        ),
-        "mean_squared_error": MetricThreshold(
-            threshold=20,  # mean_squared_error should be <= 20
-            # min_absolute_change=0.01,  # mean_squared_error should be at least 0.01 greater than baseline model accuracy
-            # min_relative_change=0.01,  # mean_squared_error should be at least 1 percent greater than baseline model accuracy
-            higher_is_better=False,
+        "accuracy": MetricThreshold(
+            threshold=0.6,  #
+            higher_is_better=True,
         ),
     }
 
