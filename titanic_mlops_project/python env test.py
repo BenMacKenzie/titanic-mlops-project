100ad433a8525e5befd0a95820f1a95fe4d23e63
BenMacKenzie <BenMacKenzie>
2024-08-06 20:40:53 +0000

    test

---
+++ titanic_mlops_project/python env test.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+## test 
+print("hello world")
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %environment
+# MAGIC "client": "1"
+# MAGIC "base_environment": ""
